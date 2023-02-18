64a403b54ca3d24d530e708afa4c5f7abf3ca3fc
myewebgit <mnislamctg7@gmail.com>
2023-02-18 23:43:48 +0600

    hover implemented

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-
-
-
-document.getElementById('t-calculation').addEventListener('click',function(){
-    const base = document.getElementById('base');
-    const height = document.getElementById('height');
+document.getElementById('t-calculation').addEventListener('click', function () {
     const area = document.getElementById('t-area');
-    const baseValue = parseFloat(base.value);
-    const heightValue = parseFloat(height.value);
-    const areaValue = 0.5*baseValue*heightValue;
-    area.innerText=areaValue;
+    const areaValue = parseFloat(0.5 * ((document.getElementById('base')).value) * ((document.getElementById('height')).value)).toFixed(2);
+    if (areaValue >= 0 ) {
+        area.innerText = areaValue;
+    }
+    else {
+        area.innerText = "Input can not be negative"
+        window.alert(area.fontcolor("red"));
+    }
 })
-document.getElementById('r-calculation').addEventListener('click',function(){
+document.getElementById('r-calculation').addEventListener('click', function () {
     const base = document.getElementById('r-base');
     const height = document.getElementById('r-height');
     const area = document.getElementById('r-area');
     const baseValue = parseFloat(base.value);
     const heightValue = parseFloat(height.value);
-    const areaValue =baseValue*heightValue;
-    area.innerText=areaValue;
+    const areaValue = baseValue * heightValue;
+    area.innerText = areaValue;
 })
