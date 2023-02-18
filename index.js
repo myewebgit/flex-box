7435597d5f0a651ed39000f27a0dabb7faac0b76
myewebgit <mnislamctg7@gmail.com>
2023-02-18 21:58:02 +0600

    function implement

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
-document.getElementById('calculation').addEventListener('click',function(){
+
+
+
+document.getElementById('t-calculation').addEventListener('click',function(){
     const base = document.getElementById('base');
     const height = document.getElementById('height');
-    const area = document.getElementById('area');
+    const area = document.getElementById('t-area');
     const baseValue = parseFloat(base.value);
     const heightValue = parseFloat(height.value);
     const areaValue = 0.5*baseValue*heightValue;
+    area.innerText=areaValue;
+})
+document.getElementById('r-calculation').addEventListener('click',function(){
+    const base = document.getElementById('r-base');
+    const height = document.getElementById('r-height');
+    const area = document.getElementById('r-area');
+    const baseValue = parseFloat(base.value);
+    const heightValue = parseFloat(height.value);
+    const areaValue =baseValue*heightValue;
     area.innerText=areaValue;
 })
